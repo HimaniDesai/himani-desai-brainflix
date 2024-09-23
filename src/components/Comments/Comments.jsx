338ab5b721280fbdb359df3d43e1a96e63100951
himanidesai <de99hi@gmail.com>
2024-09-23 16:38:03 -0400

    Adding Dynamics Trimstamp date to comments section

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,14 +1,17 @@
 import './Comments.scss'
+import ReactTimeAgo from 'react-time-ago'
 
   function Comments({comment}){
-    
+    const getDynamicTimeStamp = (date) => {
+        return (<ReactTimeAgo date={date} locale="en-US"/>)
+    }
     return (
         <div className='comment-card'>
             <div className='comment-card__img'></div>
                 <div className='comment-card-wrapper'>
                     <div className='comment-card-wrapper-top'>
                         <p className='comment-card-wrapper-top__name'>{comment.name}</p>
-                        <p className='comment-card-wrapper-top__date'>{new Date(comment.timestamp).toLocaleDateString()}</p>
+                        <p className='comment-card-wrapper-top__date'>{getDynamicTimeStamp(comment.timestamp)}</p>
                     </div>
                     <p className='comment-card-wrapper__comment'>{comment.comment}</p>
                 </div>
