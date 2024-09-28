23ed672d4ef7fa3e951c9946cf8d5e34b1b6ecd0
himanidesai <de99hi@gmail.com>
2024-09-28 14:01:44 -0400

    Adding redirect routes to Publish and Cancel buttons of Upload Page

--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -1,26 +1,20 @@
 import './Upload.scss'
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import UploadVidImage from '../../assets/images/Upload-video-preview.jpg'
 import { useState } from 'react';
+
 
 function Upload() {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
-
+    const navigate = useNavigate();
+    const handleCancel = () => {
+        navigate('/');
+    }
     const handlePublish = () => {
-        const newVideo = {
-            title,
-            description,
-        };
-
-        // axios.post(api + '/videos', newVideo)
-        //     .then(res => {
-        //         console.log(res.data);
-        //     })
-        //     .catch(err => {
-        //         console.error(err);
-        //     });
-    };
+        alert("Successfully Submitted form");
+        navigate('/');   
+    }
     return (
         <>
         <div className='upload'>
@@ -32,15 +26,15 @@ function Upload() {
                         <img className='upload__img' alt='test-thumbnail' src={UploadVidImage}></img> 
                     </div>
                     <form className='upload-form'>
-                        <label for='title' className='upload-label__title'>TITLE YOUR VIDEO</label>
+                        <label name='title' className='upload-label__title'>TITLE YOUR VIDEO</label>
                         <textarea placeholder='Add a title to your video' name='title' className='upload-form__title' onChange={(e) => setTitle(e.target.value)}></textarea>
-                        <label for='desc' className='upload-label__desc'>ADD A VIDEO DESCRIPTION</label>
+                        <label name='desc' className='upload-label__desc'>ADD A VIDEO DESCRIPTION</label>
                         <textarea placeholder='Add a description to your video' name='desc' className='upload-form__desc' onChange={(e) => setDescription(e.target.value)}></textarea>
                     </form>
                 </div>
                 <div className='upload-buttons'>
-                    <Link to={'/'}><button  className='upload-buttons__publish buttons' onClick={handlePublish}>PUBLISH</button></Link>
-                    <button className='upload-buttons__cancel buttons'>CANCEL</button> 
+                    <button  className='upload-buttons__publish buttons' onClick={handlePublish}>PUBLISH</button>
+                    <button className='upload-buttons__cancel buttons' onClick={handleCancel}>CANCEL</button> 
                 </div>
             </div>
         </div>
