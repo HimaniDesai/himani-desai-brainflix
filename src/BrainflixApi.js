0f0068de451f3d0c4dd26951895cdde85b0ea092
himanidesai <de99hi@gmail.com>
2024-09-30 16:24:25 -0400

    Adding necessary comments

--- src/BrainflixApi.js
+++ src/BrainflixApi.js
@@ -1,6 +1,7 @@
 const api = `https://unit-3-project-api-0a5620414506.herokuapp.com`
 const apikey = `?api_key=59bca329-3f10-406f-9a20-55481f50a6ae`
 const BrainflixAPI = {
+    // API URL STRINGS FOR CALL USING AXIOS
     getList: api + `/videos` + apikey,
     getVideo:  id => api + `/videos/` + `${id}` + apikey,
     getVideofile: link => `${link}` + apikey,
