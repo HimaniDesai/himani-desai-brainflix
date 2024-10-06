19f4ceecbd6b3aa3ada672be8e6c4d6af9eb0c3e
himanidesai <de99hi@gmail.com>
2024-10-06 17:33:10 -0400

    Adding Page Not Found for any incorrect routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header/Header.jsx'
 import Home from './pages/Home/Home.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Upload from './pages/Upload/Upload.jsx'
+import PageNotFound from './pages/PageNotFound/PageNotFound.jsx'
 
 function App() {
   
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<Home />}></Route>
         <Route path='/videos/:idFromParams' element={<Home />}></Route>
         <Route path='/upload' element={<Upload/>}></Route>
+        <Route path='*' element={<PageNotFound/>}></Route>
       </Routes>
     </BrowserRouter>
     </>
